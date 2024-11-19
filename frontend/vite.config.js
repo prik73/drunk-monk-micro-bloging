49e261ad721b591af9ff279563f338fd0d6b9f0d
prik73 <prinovac@gmail.com>
2024-11-20 02:04:27 +0530

    ui components added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: [".js", ".jsx"], // Ensure .jsx files are resolved
+  },
+  server:{
+    port:3000
+  }
 })
