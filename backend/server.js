ec5818c5b39aacb63ec91d72e2edd4ee1d5317b6
prik73 <prinovac@gmail.com>
2024-11-19 17:01:34 +0530

    user routes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 import express from 'express'
 import authRoutes from './routes/auth.routes.js'
+import userRoutes from './routes/user.routes.js'
+import { v2 as cloudinary } from "cloudinary"
+
 import dotenv from 'dotenv'
 import connectMongoDb from './db/connectMongoDb.js';
 import cookieParser from "cookie-parser";
@@ -12,10 +15,17 @@ app.use(express.urlencoded({ extended: true })); // to parse form ka data(encode
 app.use(cookieParser());
 dotenv.config(); 
 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
 
 
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, ()=>{
     console.log(`hello on ${PORT}`)
